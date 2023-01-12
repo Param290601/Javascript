520f7cb556704783c41dbe1ffc17cbbdf6e91562
Param290601 <paramt.mobio@gmail.com>
2023-01-12 17:04:27 +0530

    Javascript advance and basic

--- class.js
+++ class.js
@@ -13,8 +13,32 @@ class Car{
     }
 
 }
+class suv extends Car{
+    constructor(nam,height,len,model,company){
+        super(model,company);
+        this.nam = nam
+        this.height = height
+        this.len = len
+       
+        
+    }
+    feature(){
+        console.log(this.nam)
+        console.log(this.height)
+        console.log(this.len)
+        console.log(this.model)
+        console.log(this.company)
+    }
+    
+
+}
+class sedan extends Car{
+
+}
 Car.l = "under 5000mm"
 var creta = new Car("sx","hundai")
 creta.method(4,1.5,"Front-wheel")
-console.log(creta.constructor.l)
-console.log(creta.review())
+// console.log(creta.constructor.l)
+// console.log(creta.review())
+var kusaq = new suv("kusaq",2000,4500,"monte-carlo","skoda")
+console.log(kusaq.feature())
